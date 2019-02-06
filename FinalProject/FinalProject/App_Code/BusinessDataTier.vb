4928602b7d5261175c986c2fe5e5bbbd77b741fd
DrBiffness <46948448+DrBiffness@users.noreply.github.com>
2019-02-06 09:10:35 -0500

    2/6

---
+++ FinalProject/FinalProject/App_Code/BusinessDataTier.vb
@@ -0,0 +1,5 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class BusinessDataTier
+
+End Class
--- FinalProject/FinalProject/MasterPage.master.vb
+++ FinalProject/FinalProject/MasterPage.master.vb
@@ -1,5 +1,10 @@
 ﻿
 Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        If Session("Role") Is Nothing Then
+
+        End If
+    End Sub
 End Class
 
